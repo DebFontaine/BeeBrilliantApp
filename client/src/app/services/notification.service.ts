886c8826bd2ce7316a3cbf2f8e17db7e88d32fee
DebFontaine <ewo999@gmail.com>
2024-03-10 16:07:36 -0400

    Fix Azure SignalR bug

--- client/src/app/services/notification.service.ts
+++ client/src/app/services/notification.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, take } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Message } from '../models/message';
 import { User } from '../models/user';
+import * as signalR from '@microsoft/signalr';
 
 @Injectable({
   providedIn: 'root'
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     quizApiUrl: 'https://quizservice-appservice.azurewebsites.net/api/',
     reportingApiUrl: 'https://beebrilliantreportingservice.azurewebsites.net/api/',
     accountApiUrl: 'https://quizauthorizationapi.azurewebsites.net/api/',
-    hubUrl: 'https://beebrilliant.service.signalr.net/hubs/'
+    hubUrl: 'https://beebrilliantreportingservice.azurewebsites.net/hubs/'
 };
